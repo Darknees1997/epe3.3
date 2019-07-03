055cd6c551c971ede583cfb0ef55e64c6cdbfddf
Darknees1997 <38507376+Darknees1997@users.noreply.github.com>
2019-07-03 01:48:02 -0400

    Update pagina2.page.ts

--- VJuegos/src/app/pagina2/pagina2.page.ts
+++ VJuegos/src/app/pagina2/pagina2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pagina2',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pagina2Page implements OnInit {
 
-  constructor() { }
+  constructor(public menuCtrl : MenuController, public navCrtl: NavController) { }
 
   ngOnInit() {
   }
-
-}
+  menu(){
+    this.menuCtrl.toggle();
+  }
+  lol(){
+    this.navCrtl.navigateForward("page1");
+   }
+  mk(){
+    this.navCrtl.navigateForward("page2");
+    
+  }
+  ff(){
+    this.navCrtl.navigateForward("page3");
+    
+  }
+  ps5(){
+    this.navCrtl.navigateForward("page4");
+    
+  }
+  jf(){
+    this.navCrtl.navigateForward("page5");
+    
+  }
+ }
